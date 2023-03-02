5495fffc01a74bc52f570c91bb9c95a187296c29
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-03-02 23:45:05 +0200

    add import module
    import module and input feild

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+console.log(input);
